f8ff91a72dd5a8f5afe0e230ffd1889eaca5d777
sasa-glitch <salahameer200@gmail.com>
2026-01-28 22:55:57 +0200

    another one

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import About from './pages/About/About.jsx'
 import Home from './pages/Home/Home.jsx'
 import Blog from './pages/Blog/Blog.jsx'
 import NotFound from './pages/NotFound/NotFound.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, replace, RouterProvider } from 'react-router-dom'
 import BlogArticlePage from './pages/BlogArticlePage/BlogArticlePage.jsx'
 
 const routes = createBrowserRouter([
@@ -18,9 +18,9 @@ const routes = createBrowserRouter([
         {path: "about", element: <About/>},
         {path: "blog", element: <Blog/>},
         {path: "blog/:slug", element: <BlogArticlePage />},
-        {path: '*', element: <NotFound />},
+        {path: '404', element: <NotFound />},
+        {path: '*', element: <Navigate to='/404' replace />},
     ]}, 
-    {path: '*', element: <NotFound />},
 ], {
     basename: "/Adaasa"
 })
